c26d5c0da06b420ca80e3e91f8ae9e733bf02df2
Anest <anest@66ws.cc>
2019-07-25 22:17:34 -0400

    add [4]

---
+++ src/4.median-of-two-sorted-arrays/4.median-of-two-sorted-arrays.py
@@ -0,0 +1,47 @@
+#
+# @lc app=leetcode id=4 lang=python3
+#
+# [4] Median of Two Sorted Arrays
+#
+# There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+# Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+# You may assume nums1 and nums2 cannot be both empty.
+
+# Example 1:
+
+# nums1 = [1, 3]
+# nums2 = [2]
+
+# The median is 2.0
+# Example 2:
+
+# nums1 = [1, 2]
+# nums2 = [3, 4]
+
+# The median is (2 + 3)/2 = 2.5
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        # √ Accepted
+        # √ 2084/2084 cases passed (112 ms)
+        # √ Your runtime beats 46.98 % of python3 submissions
+        # √ Your memory usage beats 5.04 % of python3 submissions (14 MB)
+        nums=[]
+        i,j,il,jl=0,0,len(nums1),len(nums2)
+        while i<il and j<jl:
+            if nums1[i]<nums2[j]:
+                nums+=[nums1[i]]
+                i+=1
+            else:
+                nums+=[nums2[j]]
+                j+=1
+        if i<il:
+            nums+=nums1[i:]
+        else:
+            nums+=nums2[j:]
+        l=len(nums)
+        if l%2==0:
+            return (nums[int(l/2)]+nums[int(l/2-1)])/2 
+        else:
+            return nums[int(l/2)]
