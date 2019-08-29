fa07ec47c14fa076530c519ba4fa8f120c8d8555
Anest <anest@66ws.cc>
2019-08-28 22:32:33 -0400

    add [113]

---
+++ src/113.path-sum-ii/113.path-sum-ii.py
@@ -0,0 +1,51 @@
+#
+# @lc app=leetcode id=113 lang=python3
+#
+# [113] Path Sum II
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+# Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+
+# Note: A leaf is a node with no children.
+
+# Example:
+
+# Given the below binary tree and sum = 22,
+
+#       5
+#      / \
+#     4   8
+#    /   / \
+#   11  13  4
+#  /  \    / \
+# 7    2  5   1
+# Return:
+
+# [
+#    [5,4,11,2],
+#    [5,8,4,5]
+# ]
+# √ Accepted
+#   √ 114/114 cases passed (56 ms)
+#   √ Your runtime beats 62.29 % of python3 submissions
+#   √ Your memory usage beats 93.1 % of python3 submissions (14.6 MB)
+class Solution:
+    def pathSum(self, root: TreeNode, s: int) -> List[List[int]]:
+        if not root:
+            return []
+        res = []
+        stack = [(root, [root.val])]
+        while stack:
+            cur, ls = stack.pop()
+            if not cur.left and not cur.right and sum(ls) == s:
+                res.append(ls)
+            if cur.right:
+                stack.append((cur.right, ls+[cur.right.val]))
+            if cur.left:
+                stack.append((cur.left, ls+[cur.left.val]))
+        return res
