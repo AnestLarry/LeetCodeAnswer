aecf95d5eb738fda6d5eaaba9c51194639a88b48
Anest <anest@66ws.cc>
2020-07-31 22:11:08 -0400

    update [179]

--- src/179.largest-number/179.largest-number.py
+++ src/179.largest-number/179.largest-number.py
@@ -3,14 +3,26 @@
 #
 # [179] Largest Number
 #
-# √ Accepted
-#   √ 222/222 cases passed (56 ms)
-#   √ Your runtime beats 29.6 % of python3 submissions
-#   √ Your memory usage beats 7.14 % of python3 submissions (13.9 MB)
+# Given a list of non negative integers, arrange them such that they form the largest number.
+
+# Example 1:
+
+# Input: [10,2]
+# Output: "210"
+# Example 2:
+
+# Input: [3,30,34,5,9]
+# Output: "9534330"
+# Note: The result may be very large, so you need to return a string instead of an integer.
+import functools
 
 
 class Solution:
-    def largestNumber(self, nums: List[int]) -> str:
+    def largestNumber1(self, nums: List[int]) -> str:
+        # Accepted
+        # 222/222 cases passed (40 ms)
+        # Your runtime beats 93.96 % of python3 submissions
+        # Your memory usage beats 20 % of python3 submissions (13.7 MB)
         if not nums or nums.count(0) == len(nums):
             return "0"
         lists = []
@@ -20,7 +32,6 @@ class Solution:
             if lists:
                 inserted = False
                 for j in range(len(lists)):
-                    #if int(lists[j]+nums_i_str) < int(nums_i_str+lists[j]):
                     if lists[j]+nums_i_str < nums_i_str+lists[j]:
                         lists.insert(j, nums_i_str)
                         inserted = True
@@ -30,3 +41,20 @@ class Solution:
             else:
                 lists.append(nums_i_str)
         return "".join(lists)
+
+    def largestNumber(self, nums: List[int]) -> str:
+        # Accepted
+        # 222/222 cases passed (40 ms)
+        # Your runtime beats 93.96 % of python3 submissions
+        # Your memory usage beats 20 % of python3 submissions (13.7 MB)
+        if not any(nums):
+            return "0"
+        return "".join(
+            sorted(
+                map(str, nums),
+                key=functools.cmp_to_key(
+                    lambda n1, n2: -1 if n1 + n2 > n2 +
+                    n1 else (1 if n1+n2 < n2+n1 else 0)
+                )
+            )
+        )
