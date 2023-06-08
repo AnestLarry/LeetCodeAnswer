8cf6fb6c36466cf5a495c1d5910f9c5646b9908e
Anest <32736719+AnestLarry@users.noreply.github.com>
2023-06-08 06:04:31 -0400

    update [1]

--- src/1.two-sum/1.two-sum.cs
+++ src/1.two-sum/1.two-sum.cs
@@ -13,27 +13,20 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
  */
-public class Solution
-{
-    public int[] TwoSum(int[] nums, int target)
-    {
+public class Solution {
+    public int[] TwoSum (int[] nums, int target) {
         // Accepted
-        // 29/29 cases passed (324 ms)
-        // Your runtime beats 73.7 % of csharp submissions
-        // Your memory usage beats 5.33 % of csharp submissions (30.3 MB)
-        Dictionary<int, int> m = new Dictionary<int, int>();
-        for (int i = 0; i < nums.Length; i++)
-        {
-            if (m.ContainsKey(target - nums[i]))
-            {
+        // 29/29 cases passed (280 ms)
+        // Your runtime beats 95.59 % of csharp submissions
+        // Your memory usage beats 33.77 % of csharp submissions (31 MB)
+        Dictionary<int, int> m = new Dictionary<int, int> ();
+        for (int i = 0; i < nums.Length; i++) {
+            if (m.ContainsKey (target - nums[i])) {
                 return new int[] { m[target - nums[i]], i };
-            }
-            else
-            {
+            } else {
                 m[nums[i]] = i;
             }
         }
-        return new int[] { -1, -1 };
+        return new int[] {-1, -1 };
     }
 }
-
